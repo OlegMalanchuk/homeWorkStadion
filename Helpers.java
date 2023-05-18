e2632cea79876cddf027ac18f6002f16c53aed2a
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-18 19:44:33 +0300

    Create Helpers.java

---
+++ Helpers.java
@@ -0,0 +1,39 @@
+public class Helpers {
+
+    public static void choiseHelper(){
+        System.out.println("1) купити квитки ");
+        System.out.println("2) відвідати бар ");
+        System.out.println("3) піти на віп міся ");
+        System.out.println("4) поставити на команду ");
+    }
+    public static void choiseSector(){
+        System.out.println("сектор 1");
+        System.out.println("сектор 2");
+        System.out.println("сектор 3");
+        System.out.println("сектор 4");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
