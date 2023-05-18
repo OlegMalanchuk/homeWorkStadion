b38d331100cf63a83264b74debfd9e37cff17fba
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-18 19:44:40 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        Stadium stadium = new Stadium("Lviv");
+        stadium.getNameStadium();
+        stadium.choice();
+        BuyTickets buyTickets =new BuyTickets();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
